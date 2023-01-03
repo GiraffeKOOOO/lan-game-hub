5dd2da9b3e33bf188a721189ee70ca47fe9dc2c3
Pascal Stepien <pstepien@hotmail.co.uk>
2023-01-03 23:58:34 +0000

    made a dark mode switch and a login form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ import PlayerTeamButton from "../components/PlayerTeamButton";
 import PlayerTeamList from "../components/PlayerTeamList";
 
 function Home() {
-  const { selectedGame, moreInfoHidden, playerTeamButtonHidden } = useContext(GameContext);
+  const { selectedGame, moreInfoHidden, playerTeamButtonHidden, darkMode } = useContext(GameContext);
 
   return (
     <>
@@ -32,7 +32,7 @@ function Home() {
         </div>
 
         {/** right panel: main body of the page */}
-        <div id="main-body" className="col-span-6">
+        <div id="main-body" className="col-span-6" style={darkMode ? { backgroundColor: "#FFFFFF" } : { backgroundColor: "#000000" }}>
           <div id="main-body-container" className="grid grid-flow-row">
             <TitleBar />
             <div id="timeline" className="overflow-hidden border-y-2">
