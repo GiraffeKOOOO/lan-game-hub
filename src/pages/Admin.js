177a5e86ac13240eb7908b8df5726cb01a6486a0
Pascal Stepien <pstepien@hotmail.co.uk>
2023-01-03 19:13:33 +0000

    starting work on the admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -2,6 +2,12 @@ import { useContext, useState } from "react";
 import "../App.css";
 
 function Admin() {
+  // checkAuth()
+
+  // if true log in take user to admin page
+
+  // else redirect to log in page
+
   return (
     <>
       <h1>Admin page</h1>
